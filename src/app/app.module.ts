b74161de6059e57a699d75226e22912485c6c099
serzhikkk <sergey-andreev-97-97@mail.ru>
2017-10-10 19:37:29 +0300

    Add header component with styles

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,9 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 
+import { HeaderComponent } from './layouts/header/header.component';
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule
